927bceb26e4f84162086571542ac8da624d5cc7f
Tecnico - TI <tecnico@epfserver.com.br>
2019-06-17 22:11:19 -0300

    criacao do projeto

---
+++ src/main/java/br/com/fundatec/lancheria/LancheriaDaniela/LancheriaDanielaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fundatec.lancheria.LancheriaDaniela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancheriaDanielaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancheriaDanielaApplication.class, args);
+	}
+
+}
